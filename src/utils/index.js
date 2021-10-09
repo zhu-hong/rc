cb9e23685efaf25ded1fb060aa903ca777e207da
zhu-hong <zhxhyy@qq.com>
2021-10-10 07:56:03 +0800

    记忆功能,捕获每个对象状态,改动时将list写入缓存

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,28 @@
 import { list } from '../data'
+import { customRef } from '@vue/reactivity'
 
 const changeStatus = (id, status) => {
   list.find(item => item.id === id).status = status;
 }
 
+const catchStatus = (value, cache) => {
+  return customRef((track, trigger) => {
+    return {
+      get(){
+        track();
+        return value;
+      },
+      set(newVal){
+        value = newVal;
+        trigger();
+        localStorage.setItem('cache', JSON.stringify(cache));
+      }
+    }
+  })
+  
+}
+
 export {
-  changeStatus
+  changeStatus,
+  catchStatus,
 }
