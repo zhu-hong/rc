43594e7ad29af665bc26f7e1e36204d9ccbf4963
zhu-hong <zhxhyy@qq.com>
2021-11-12 10:47:02 +0800

    fix filter function production error

--- src/components/FilterInput/index.vue
+++ src/components/FilterInput/index.vue
@@ -1,15 +1,19 @@
 <script setup>
+import { shallowRef } from '@vue/reactivity';
+
 defineProps({
   keyword: String,
 })
+
+const key = shallowRef('');
 </script>
 
 <template>
   <div class="relative w-80 mx-auto mb-4">
     <input
       class="w-full p-3 pr-12 bg-gray-900 text-white font-bold rounded-lg outline-none transition focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-      v-model="keyword"
-      @input="$emit('update:keyword', keyword)"
+      v-model="key"
+      @input="$emit('update:keyword', key)"
       autofocus
     />
     <transition name="x">
